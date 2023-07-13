7d5c57aa11ef3b1f46ecb2dbf82cfdee39c8beea
Jonathan Soto Aguilar <jonathan.soto.a@usach.cl>
2023-07-13 19:38:30 -0400

    fix(Usuario): :sparkles: Cambio Entidad Voluntario y Usuario

--- Backend/src/main/java/Grupo4TBD/VoluntariosTBD/Models/Requerimiento.java
+++
@@ -1,9 +0,0 @@
-package Grupo4TBD.VoluntariosTBD.Models;
-
-import Grupo4TBD.VoluntariosTBD.Entities.Voluntario;
-import lombok.Data;
-
-@Data
-public class Requerimiento extends Voluntario {
-    private Integer cant_requerimientos;
-}
